76f2d776c3866a4aa098ad79ce6f08b9ae638773
Natalya Colburn <greatqueenbee@gmail.com>
2022-07-31 13:18:03 -0600

    changes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -29,28 +29,9 @@ export const ADD_REMINDER = gql`
     addReminder(remindertext: $reminderText) {
       _id
       reminderText
-      reminderAuthor
       createdAt
-      comments {
-        _id
-        commentText
-      }
+      
     }
   }
 `;
 
-export const ADD_COMMENT = gql`
-  mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        createdAt
-      }
-    }
-  }
-`;
