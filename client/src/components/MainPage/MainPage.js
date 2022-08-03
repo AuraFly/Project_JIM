0dd524079c662b5aa172348e78e236dc7d36e00d
Natalya Colburn <greatqueenbee@gmail.com>
2022-08-03 11:07:12 -0600

    login page

---
+++ client/src/components/MainPage/MainPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container } from 'rect-bootstrap';
+
+const MainPage = ({title, children}) => {
+  return (
+    <div className='mainback'>
+        <Container>
+            <Row>
+           <div className='page'>
+            { title && (
+            <>
+            <h1 className='heading'>{ title }</h1>
+            <hr />
+            </>
+
+           ) }
+           {children}
+            </div>     
+            </Row>
+        </Container>
+        
+    </div>
+  )
+}
+
+export default MainPage
--- client/src/components/pages/LoginPage/LoginPage.js
+++ client/src/components/pages/LoginPage/LoginPage.js
@@ -1,9 +1,93 @@
-import React from 'react'
+import axios from 'axios';
+import React from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import MainPage from '../../MainPage/MainPage';
+import "./loginScreen.css"; 
+import axios from "axios";
+
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
+
+
+const {email, setEmail} = useState("")
+const {password, setPassword} = useState("");
+const {error, setError} = useState(false);
+const {loading, setLoading} = useState(false);
+
+const submitHandler = async (e) => {
+e.preventDefault();
+
+try  {
+  const config = {
+    headers: {
+      "Content-type":"application/json"
+    }
+  }
+setLoading(true)
+
+const { data } = await axios.post(
+  '/api/users/login',
+  {
+    email, 
+    password,
+  },
+  config
+  );
+
+  console.log(data);
+  localStorage.setItem("userInfo", JSON.stringify(data));
+setLoading(false);
+
+} catch (error) {
+  setError(error.response.data.message);
 }
+
+};
+
+
+
+
+
+
+  return (
+    <MainPage title="LOGIN">
+      <div className="loginContainer">
+        <Form on onSubmit={submitHandler}>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label> Email Address </Form.Label>
+            <Form.controlId
+            type="email"
+            value={email}
+            placeholder="Enter Email Address"/>
+            onChange={(e) => setEmail(e.target.value)}
+
+          </Form.Group>
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label> Password </Form.Label>
+      <Form.Control 
+      type="password"
+      value={password}
+      placeholder="Password"
+      onChange={(e) => setPassword(e.target.value)}
+      />
+
+      </Form.Group>
+
+<Button varient="primary" type="submit"> Submit </Button>
+          </Form> 
+
+     <Row className="py-3">
+      <Col>
+      Are you New? <link to="/register">Register Here</link>
+      </Col>
+      </Row>     
+       
+    </div>
+    
+    </MainPage>
+    
+  );
+};
 
 export default LoginPage
