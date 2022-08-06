98514d7092783de3641c29f079400c4abb1d3380
Jesus Rodriguez <jesusrodriguez7@comcast.net>
2022-08-05 19:04:43 -0600

    fixed login logic, some mutations for login and typedefs, contributors JR and NC

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Typed from 'react-typed';
+// import Typed from 'react-typed';
 
 const Aboutme = ({ currentPage, handlePageChange }) => {
   return (
@@ -10,20 +10,20 @@ const Aboutme = ({ currentPage, handlePageChange }) => {
         </h1>
         <div className='flex justify-center items-center'>
           <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-          Tired of the hassle involved when making an invoice?
+            Tired of the hassle involved when making an invoice?
           </p>
-          </div>
-          <Typed
+        </div>
+        {/* <Typed
           className='md:text-2xl text-xl font-bold text-gray-500'
             strings={['Let J.I.M. do the work for you!']}
             typeSpeed={120}
             backSpeed={140}
             loop
-          />
+          /> */}
         <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'><a href="#Login" onClick={() => handlePageChange("Login")}
-            className={
-              currentPage === "#Login" ? "nav-link active" : "nav-link"
-            }>Log in or Sign up</a></button>
+          className={
+            currentPage === "#Login" ? "nav-link active" : "nav-link"
+          }>Log in or Sign up</a></button>
       </div>
     </div>
   );
